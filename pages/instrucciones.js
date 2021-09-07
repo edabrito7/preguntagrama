44d729616526d36ffba719c3b777634ea090dbc6
edabrito7 <eabbrito1996@gmail.com>
2021-09-07 14:39:43 -0400

    add instruction page

--- pages/instrucciones.js
+++ pages/instrucciones.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 
+// components
+import Title from 'components/commons/Title'
+
+// icons
+import Clock from 'components/icons/clock'
+import Life from 'components/icons/life'
 
 export default function Instrucciones () {
     return (
@@ -7,9 +13,64 @@ export default function Instrucciones () {
             <Head>
                 <title>Instrucciones</title>
             </Head>
-            <section>
-                <h1>Instrucciones</h1>
-            </section>
+            <main
+            className='mx-4'
+            >
+                <Title/>
+                <section 
+                className='my-2'
+                >
+                    <h2
+                    className='font-bold text-lg'
+                    >Idea general</h2>
+                    <p
+                    className='mt-4'
+                    >
+                        El juego consiste en responder preguntas sobre teoría o lenguaje musical.
+                        Existe diversas preguntas agrupadas en categorías, algunas de ellas son:
+                    </p>
+                    <ul 
+                    className='mt-2 mb-4 mx-4'
+                    >
+                        <li className='list-disc'>Escalas</li>
+                        <li className='list-disc'>Arpegios</li>
+                        <li className='list-disc'>Figuras</li>
+                    </ul>
+                    <h2
+                    className='font-bold text-lg'
+                    >¿Tengo un máximo de tiempo para responder?</h2>
+                    <p
+                    className='my-4'
+                    >
+                        Si, cada pregunta tiene un tiempo máximo para responderse de 30 segundos. 
+                        El tiempo se puede extiende 15 segundos al hacer click en el icono del reloj en la parte inferior-derecha de cada pregunta.
+                    </p>
+                    <figure
+                    className='flex justify-center items-center my-8'
+                    >
+                        <Clock/>
+                    </figure>
+                    <h2
+                    className='font-bold text-lg'
+                    >¿Qué pasa si me equivoco mucho?</h2>
+                    <p
+                    className='my-4'
+                    >
+                        Perderás vidas. Al empezar tienes un total de 3 vidas, por cada pregunta incorrecta pierdes una vida,
+                        también pierdes una vida si se te acaba el tiempo. 
+                        Si te quedas sin vidas debes comenzar desde el principio &#128542;.
+                    </p>
+                    <figure
+                    className='flex justify-center items-center my-8'
+                    >
+                        <Life />
+                        <Life />
+                        <Life
+                        stroke='#00000029'
+                        />
+                    </figure>
+                </section>
+            </main>
         </>
     )
 }
